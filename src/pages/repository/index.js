1dd5cea451db9e4c311d6f368355d1c3d1d2a1dc
Breno Henrique <bhfs.2908@gmail.com>
2021-08-28 11:46:36 -0300

    feat:add routes

---
+++ src/pages/repository/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+const Repository = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
